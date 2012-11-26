e7a676d9ce39ad5f57b95690f4d5cca4c57f3466
matheus.danemberg <fixedbyus@matheus.danemberg>
2012-11-26 18:10:41 -0200

    new repository

---
+++ system.js
@@ -0,0 +1,24 @@
+// Player's Class
+function Player (name) {
+	this._name = name;
+	this.team = '';
+	this.points = 0;
+}
+
+// Logic tournament
+function Tournament () {
+	this.players = [];
+}
+
+Tournament.prototype.newGame = function(player1, player2) {
+	var game = ['player1', 'player2'];
+	for (var i = players.length - 1; i >= 0; i--) {
+		if (players[i]._name === player1) {
+			game['player1'] = players[i];
+		}else if (players[i]._name === player2) {
+			game['player2'] = players[i];
+		}
+	}
+	
+
+};
